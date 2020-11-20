8c502e5d2b949de2d93943a7a51fec8a7df64028
pcl <pcl@pcldeMacBook-Pro.local>
2020-11-20 11:15:03 +0800

    1

--- 1.js
+++ 1.js
@@ -1,10 +1,22 @@
-for (let hh of Object.getOwnPropertyNames(window)) {
-  try {
-    if (
-      typeof window[hh] === "function" &&
-      window[hh].prototype instanceof HTMLElement
-    ) {
-      console.log(hh);
-    }
-  } catch (e) {}
-}
+// for (let hh of Object.getOwnPropertyNames(window)) {
+//   try {
+//     if (
+//       typeof window[hh] === "function" &&
+//       window[hh].prototype instanceof HTMLElement
+//     ) {
+//       console.log(hh);
+//     }
+//   } catch (e) {}
+// }
+
+let paths = [
+  ["London", "New York"],
+  ["New York", "Lima"],
+  ["Lima", "Sao Paulo"],
+];
+let newArry = [];
+let pathsArry = paths.forEach((v) => {
+  newArry.push(...v);
+});
+
+let end = Array.from(new Set(newArry));
